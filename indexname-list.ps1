c3792c268d79323799083469b19c13993003dca7
DArcy Simanton <djsimanton@gmail.com>
2025-04-25 14:04:45 -0700

    Auto update 2025-04-25_14-04-44

---
+++ indexname-list.ps1
@@ -0,0 +1,7 @@
+﻿$folder = "C:\Users\D'Arcy\Dropbox\Publishing\App building\hmong\www-iphone\index"
+$outputFile = "$env:USERPROFILE\Desktop\filelist.txt"
+
+Get-ChildItem -Path $folder -Filter *.html -Recurse | ForEach-Object {
+    $relativePath = $_.FullName.Replace($folder, "").Replace("\", "/").TrimStart("/")
+    "'/index/$relativePath',"
+} | Set-Content $outputFile
