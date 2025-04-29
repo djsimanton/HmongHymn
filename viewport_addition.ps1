867c90120ace19d0aa2e314b9fc5fae005951e03
DArcy Simanton <djsimanton@gmail.com>
2025-04-29 16:42:40 -0700

    Auto update 2025-04-29_16-42-40

---
+++ viewport_addition.ps1
@@ -0,0 +1,27 @@
+﻿# Get the folder where the script is located
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Define the line to insert after
+$insertAfterPattern = '<meta name="keywords"'
+
+# Define the line to insert
+$viewportMeta = '<meta name="viewport" content="width=device-width, initial-scale=1.25, maximum-scale=1.25, user-scalable=no">'
+
+# Get all .html files in the folder (non-recursive)
+Get-ChildItem -Path $scriptFolder -Filter *.html -File | ForEach-Object {
+    $file = $_.FullName
+    $lines = Get-Content $file
+    $newLines = @()
+
+    foreach ($line in $lines) {
+        $newLines += $line
+        if ($line -match $insertAfterPattern) {
+            $newLines += $viewportMeta
+        }
+    }
+
+    # Save updated file
+    Set-Content -Path $file -Value $newLines -Encoding UTF8
+}
+
+Write-Output "Viewport tag added to applicable HTML files."
